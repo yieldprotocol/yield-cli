5715705103567d78e185a6774b0625d6be0d1a96
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-10-28 10:05:06 +0000

    feat: added additional pool stats

--- src/tvl.js
+++ src/tvl.js
@@ -15,6 +15,7 @@
 // - fyDaiLP21Sep: The address of the fyDaiLP21Sep contract
 // - fyDaiLP21Dec: The address of the fyDaiLP21Dec contract
 const { ethers, BigNumber } = require('ethers')
+const fmtEth = ethers.utils.formatEther
 
 // defaults to the infura node
 const ENDPOINT = process.env.ENDPOINT || 'https://mainnet.infura.io/v3/878c2840dbf943898a8b60b5faef8fe9'
@@ -22,32 +23,42 @@ const ENDPOINT = process.env.ENDPOINT || 'https://mainnet.infura.io/v3/878c2840d
 const VAT = process.env.VAT || '0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B'
 const TREASURY = process.env.TREASURY || '0xFa21DE6f225c25b8F13264f1bFF5e1e44a37F96E'
 const series = {
-    'Oct 2020' : {
+    'October 2020' : {
+        fyDai : process.env.fyDai20Oct || "0x92c25C17C0C908E52b16627F353F1004543f8A32",
         pool : process.env.fyDaiLP20Oct || "0x6feb7B2a023C9Bc3ccCdF7c5B5a7b929B9a65E04",
     },
-    'Dec 2020' : {
+    'December 2020' : {
+        fyDai : process.env.fyDai20Dec || "0xF2C9c61487D796032cdb9d57f770121218AC5F91",
         pool : process.env.fyDaiLP20Dec || "0xF7dB19E0373937A13e4b12287B1C121Dfb2d9BF8",
     },
-    'Mar 2021' : {
+    'March 2021' : {
+        fyDai : process.env.fyDai21Mar || "0x3DeCA9aF98F59eD5125D1F697aBAd2aF45036332",
         pool : process.env.fyDaiLP21Mar || "0xb39221E6790Ae8360B7E8C1c7221900fad9397f9",
     },
-    'Jun 2021' : {
+    'June 2021' : {
+        fyDai : process.env.fyDai21Jun || "0xe523442a6c083016E2F430ab0780250ef4438536",
         pool : process.env.fyDaiLP21Jun || "0x250f8d88173E0D9b692A9742f54e87E01A9FA54E",
     },
-    'Sep 2021' : {
+    'September 2021' : {
+        fyDai : process.env.fyDai21Sep || "0x269A30E0fD5231438017dC0438f818A80dC4464B",    
         pool : process.env.fyDaiLP21Sep || "0x8EcC94a91b5CF03927f5eb8c60ABbDf48F82b0b3",
     },
-    'Dec 2021' : {
+    'December 2021' : {
+        fyDai : process.env.fyDai21Dec || "0x9D7e85d095934471a2788F485A3c765d0A463bD7",
         pool : process.env.fyDaiLP21Dec || "0x5591f644B377eD784e558D4BE1bbA78f5a26bdCd",
     },
 }
 
 const ETH = ethers.utils.formatBytes32String("ETH-A")
 const RAY = BigNumber.from(10).pow(BigNumber.from(27))
-const WAD = BigNumber.from(10).pow(BigNumber.from(18))
 
 const mulRay = (x, ray) => {
     return (BigNumber.from(x)).mul(BigNumber.from(ray)).div(RAY)
+}
+
+const toDate = (ts)=> {
+    const date = new Date(ts * 1000)
+    return `${date.getMonth()}/${date.getFullYear()}`
 }
 
 const VAT_ABI = [
@@ -62,6 +73,14 @@ const TREASURY_ABI = [
 
 const POOL_ABI = [
     "function getDaiReserves() view returns (uint128)",
+    "function getFYDaiReserves() view returns (uint128)",
+    "function totalSupply() view returns (uint256)",
+    "function fyDai() view returns (address)",
+]
+
+const FYDAI_ABI = [
+    "function maturity() view returns (uint256)",
+    "function balanceOf(address) view returns (uint256)",
 ]
 
 ;(async () => {
@@ -75,19 +94,27 @@ const POOL_ABI = [
   const eth = BigNumber.from((await vat.urns(ETH, treasury.address))[0])
   const spot = BigNumber.from((await vat.ilks(ETH))[2])
   const collateral = mulRay(eth, spot)
-  console.log(`Treasury Eth:   ${collateral.div(WAD)} (${eth.div(WAD)} Eth)`)
+  console.log(`Treasury Eth:   ${fmtEth(collateral).split('.')[0]} (${fmtEth(eth).split('.')[0]} Eth)`)
 
   const savings = await treasury.savings()
   const debt = await treasury.debt()
-  console.log(`Treasury Dai:   ${savings.div(WAD)}`)
-  console.log(`Treasury Debt:  ${debt.div(WAD)}`)
+  console.log(`Treasury Dai:   ${fmtEth(savings).split('.')[0]}`)
+  console.log(`Treasury Debt:  ${fmtEth(debt).split('.')[0]}`)
 
   let tvl = savings.add(collateral).sub(debt)
   for (name in series) {
     const pool = new ethers.Contract(series[name]['pool'], POOL_ABI, provider)
+    const fyDai = new ethers.Contract(series[name]['fyDai'], FYDAI_ABI, provider)
     const dai = await pool.getDaiReserves()
-    console.log(`${name}:       ${dai.div(WAD)}`)
+    const timeToMaturity = Math.floor(await fyDai.maturity() - (Date.now() / 1000))
+    console.log()
+    console.log(`${name}`)
+    console.log(`Dai Reserves:       ${fmtEth(dai).split('.')[0]}`)
+    console.log(`FYDai Reserves:     ${fmtEth(await pool.getFYDaiReserves()).split('.')[0]} (${fmtEth(await fyDai.balanceOf(pool.address)).split('.')[0]})`)
+    console.log(`Token Supply:       ${fmtEth(await pool.totalSupply()).split('.')[0]}`)
+    console.log(`Time to Maturity:   ${timeToMaturity}`)
     tvl = tvl.add(dai)
   }
-  console.log(`TVL:            ${tvl.div(WAD)}`)
+  console.log()
+  console.log(`TVL:                ${fmtEth(tvl).split('.')[0]}`)
 })()
